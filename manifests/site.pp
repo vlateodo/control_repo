551083aa42b2cdecb0302b41f41917a42468bc2f
vlateodo <65283717+vlateodo@users.noreply.github.com>
2020-05-13 16:42:04 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,9 @@
 node default {
-}
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'puppet-master' {
   include role::master_server
